78217d455825c830c0bfb8f3067427cdefd3cd32
Yushiro <yushiro43@protonmail.com>
2018-12-06 22:46:35 -0600

    Refactor Day 6

--- day6.rkt
+++ day6.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (define input (file->lines "day6-input.txt"))
-;(define input (string-split "1, 3\n4, 5\n3, 1\n6, 7\n5, 5\n" "\n"))
+;(define input (string-split "1, 1\n10, 1\n1, 10\n10, 10\n5, 5" "\n"))
 
 (define point-names (map string (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")))
 
@@ -12,31 +12,34 @@
 (define max-x (apply max (map car points)))
 (define max-y (apply max (map cadr points)))
 
-(define the-map (make-vector (add1 max-x) (vector)))
+(define border-size 8)
+
+(define (translate xy)
+  (+ (sub1 xy) border-size))
+
+(define (make-map)
+  (apply vector (map (lambda (_)
+                       (make-vector (+ max-y (* border-size 2)) " "))
+                     (range (+ max-x (* border-size 2))))))
+
+(define (fill-map)
+  (for ([point points]
+        [point-name point-names])
+       (match-let ([(list x y) point])
+                  (let ([row (vector-ref the-map (translate x))])
+                    (vector-set! row (translate y) point-name)
+                    (vector-set! the-map (translate x) row)
+                    (hash-set! the-points point-name point)))))
+
+(define the-map (make-map))
 (define the-points (make-hash))
-
-; place points on map
-(for ([point points]
-      [point-name point-names])
-     (match-let ([(list x y) point])
-                (let* ([row (vector-ref the-map x)]
-                       [row (if (= 0 (vector-length row)) (make-vector (add1 max-y) " ") row)])
-                  (vector-set! row y point-name)
-                  (vector-set! the-map x row)
-                  (hash-set! the-points point-name point))))
-
-; extend rows that are too short
-(for ([row the-map]
-      [i (in-range (add1 max-x))])
-     (if (= 0 (vector-length row))
-       (vector-set! the-map i (make-vector (add1 max-y) " "))
-       '()))
+(fill-map)
 
 (define (get-point-distances-from x y)
   (for/list ([point (hash->list the-points)])
             (let* ([name (car point)]
-                   [px (cadr point)]
-                   [py (caddr point)]
+                   [px (translate (cadr point))]
+                   [py (translate (caddr point))]
                    [distance (+ (abs (- px x)) (abs (- py y)))])
               (list name distance))))
 
@@ -49,15 +52,15 @@
       (values "." '())
       (values (car closest1) (cadr closest1)))))
 
-(for ([row the-map]
-      [x (in-range (add1 max-x))])
-     (for ([value row]
-           [y (in-range (add1 max-y))]
-           #:when (string=? " " value))
-          (let-values ([(point-name distance) (find-closest-point x y)])
-                      (vector-set! row y point-name))))
+(define (find-areas)
+  (for ([row the-map]
+        [x (in-range (+ max-x (* border-size 2)))])
+       (for ([value row]
+             [y (in-range (+ max-y (* border-size 2)))]
+             #:when (string=? " " value))
+            (let-values ([(point-name distance) (find-closest-point x y)])
+                        (vector-set! row y point-name))))
 
-(define areas
   (for*/fold ([area (hash)])
              ([point-name (hash-keys the-points)]
               [row the-map]
@@ -66,11 +69,9 @@
                (hash-update area point-name add1 0)
                area)))
 
-; 3871 -- I printed the whole list and guessed until I found the right one ;-)
-(println (sort (hash-values areas) <))
-
-; print map
-#;(for ([row the-map])
-     (for ([pos row])
-          (display pos))
-     (displayln ""))
+(define areas1 (hash-values (find-areas)))
+(set! border-size 10)
+(set! the-map (make-map))
+(fill-map)
+(define areas2 (hash-values (find-areas)))
+(printf "largest non-infinite area: ~s\n" (last (sort (set-intersect areas1 areas2) <)))
