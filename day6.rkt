1c7b5a84f462ad089cd19de8a091c6a202084147
Yushiro <yushiro43@protonmail.com>
2018-12-07 19:06:45 -0600

    Refactor Day 6

--- day6.rkt
+++ day6.rkt
@@ -5,14 +5,13 @@
 
 (define point-names (map string (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")))
 
-(define points
-  (for/list ([line input])
-            (map string->number (string-split line ", "))))
+(define coords (for/list ([line input]) (map string->number (string-split line ", "))))
+(define points (make-hash (for/list ([c coords] [n point-names]) (cons n c))))
 
-(define max-x (apply max (map car points)))
-(define max-y (apply max (map cadr points)))
+(define max-x (apply max (map first (hash-values points))))
+(define max-y (apply max (map last (hash-values points))))
 
-(define border-size 8)
+(define border-size 0)
 
 (define (translate xy)
   (+ (sub1 xy) border-size))
@@ -22,21 +21,18 @@
                        (make-vector (+ max-y (* border-size 2)) " "))
                      (range (+ max-x (* border-size 2))))))
 
-(define (fill-map)
-  (for ([point points]
-        [point-name point-names])
-       (match-let ([(list x y) point])
-                  (let ([row (vector-ref the-map (translate x))])
-                    (vector-set! row (translate y) point-name)
-                    (vector-set! the-map (translate x) row)
-                    (hash-set! the-points point-name point)))))
-
-(define the-map (make-map))
-(define the-points (make-hash))
-(fill-map)
+(define (build-map points)
+  (let ([the-map (make-map)])
+    (for ([point (hash-values points)]
+          [point-name point-names])
+         (match-let ([(list x y) point])
+                    (let ([row (vector-ref the-map (translate x))])
+                      (vector-set! row (translate y) point-name)
+                      (vector-set! the-map (translate x) row))))
+    the-map))
 
 (define (get-point-distances-from x y)
-  (for/list ([point (hash->list the-points)])
+  (for/list ([point (hash->list points)])
             (let* ([name (car point)]
                    [px (translate (cadr point))]
                    [py (translate (caddr point))]
@@ -52,7 +48,7 @@
       (values "." '())
       (values (car closest1) (cadr closest1)))))
 
-(define (find-areas)
+(define (find-areas the-map)
   (for ([row the-map]
         [x (in-range (+ max-x (* border-size 2)))])
        (for ([value row]
@@ -62,16 +58,45 @@
                         (vector-set! row y point-name))))
 
   (for*/fold ([area (hash)])
-             ([point-name (hash-keys the-points)]
+             ([point-name (hash-keys points)]
               [row the-map]
               [pos row])
              (if (string=? pos point-name)
                (hash-update area point-name add1 0)
                area)))
 
-(define areas1 (hash-values (find-areas)))
-(set! border-size 10)
-(set! the-map (make-map))
-(fill-map)
-(define areas2 (hash-values (find-areas)))
-(printf "largest non-infinite area: ~s\n" (last (sort (set-intersect areas1 areas2) <)))
+(define (fill-distance-totals the-map)
+  (for ([row the-map]
+        [x (in-range (add1 max-x))])
+       (for ([value row]
+             [y (in-range (add1 max-y))])
+            (let ([sum (for/sum ([pd (get-point-distances-from x y)])
+                                (match-let ([(list _ distance) pd])
+                                           distance))])
+              (vector-set! row y sum))))
+  the-map)
+
+(define (safe-region-size the-map)
+  (for/sum ([row the-map]
+            [x (in-range (add1 max-x))])
+           (for/sum ([pos row]
+                     [y (in-range (add1 max-y))])
+                    (if (< pos 10000)
+                      1
+                      0))))
+
+(set! border-size 8)
+(let* ([map1 (build-map points)]
+       [areas1 (hash-values (find-areas map1))])
+  ; run it again with a different border size
+  ; we'll assume areas that change are "infinite"
+  (set! border-size 10)
+  (let* ([map2 (build-map points)]
+         [areas2 (hash-values (find-areas map2))]
+         [non-changing (set-intersect areas1 areas2)])
+    (printf "largest non-infinite area: ~s\n" (last (sort non-changing <)))))
+
+(set! border-size 0)
+(let* ([map3 (fill-distance-totals (build-map points))]
+       [size (safe-region-size map3)])
+  (printf "safe region: ~s\n" size))
